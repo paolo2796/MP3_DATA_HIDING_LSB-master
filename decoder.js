a85cd963555dc21cc159081522a0a959f78be6ca
paul <p.vigorito3@studenti.unisa.it>
2019-12-09 11:09:55 +0100

    implementazione encoder header bit substitute

--- decoder.js
+++ decoder.js
@@ -23,18 +23,13 @@ fs.readFile(pathToMp3, (error, buffer) => {
         console.error("" + error);
         process.exit(1);
     }
-    var buf_dataview = new DataView(toArrayBuffer(buffer));
-    const tags = mp3Parser.readTags(buf_dataview);
-    var dataFrame = mp3Parser.readFrame(buf_dataview,tags[tags.length-1]._section.nextFrameIndex);
-    var count=0;
+    let buf_dataview = new DataView(toArrayBuffer(buffer));
+    let tags = mp3Parser.readTags(buf_dataview);
+    let dataFrame = mp3Parser.readFrame(buf_dataview,tags[tags.length-1]._section.nextFrameIndex);
+    let count=0;
     while(dataFrame != null && mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex) != null) {
         dataFrame = mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex);
-        if(dataFrame.header.frameIsPadded && dataFrame.header.privateBit==1){
-            count++;
-            var sliceBuf = buffer.slice(dataFrame._section.offset,dataFrame._section.nextFrameIndex);
-            var b_dataview = new DataView(toArrayBuffer(sliceBuf));
-            console.log(b_dataview.getUint8(sliceBuf.length-1));
-        }
+        
     }
     console.log(count);
 
--- encoder.js
+++ encoder.js
@@ -3,10 +3,15 @@
 const fs = require("fs");
 const util = require("util");
 const mp3Parser = require("mp3-parser");
-const bitwise = require("bitwise");
 const bw = require ("buffered-writer");
+const BitArray = require('node-bitarray')
 
 
+
+const two_bit1 = "00";
+const two_bit2 = "01";
+const two_bit3 = "10";
+const two_bit4 = "11";
 
 const pathMp3Stego = "audio_stego.mp3";
 const pathToMp3 = process.argv[2];
@@ -44,29 +49,35 @@ fs.readFile(pathToMp3, (error, buffer) => {
     newBuffer = Buffer.concat([sliceBuf]);
     var dataFrame = mp3Parser.readFrame(buf_dataview,tags[tags.length-1]._section.offset);
 
-    if(!check_max_size_embed_byte(dataFrame,secret_message,buf_dataview)){
+    if(!check_max_embedbyte(dataFrame,secret_message,buf_dataview)){
         console.warn("Il messaggio è troppo grande per essere iniettato nel file audio corrente");
         console.warn("TOT FRAME " + totframes );
-        console.warn("TOT FRAME UNPADDED " + totframes_unpadded);
+        //console.warn("TOT FRAME UNPADDED " + totframes_unpadded);
         process.exit(1);
     }
-
-    console.log("TOT FRAME " + totframes );
-    console.log("TOT FRAME UNPADDED " + totframes_unpadded);
-    console.log(tags);
-    var secret_message_arr = Array.from(secret_message);
+    console.log("TOT FRAMES " + totframes );
+    console.log("PROCESSAMENTO IN CORSO ...");
+    var message_bits = convert_to_binary(secret_message);
     var i=0;
     while(dataFrame!=null && mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex) != null) {
+        /* start substitute_header_bit_unused */
+        substituteHeaderBit(dataFrame,buffer,message_bits[i]);
+        dataFrame = mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex);
+        i++;
+
+        /* end substitute_header_bit_unused */
+
+        /* padding stuff
         if(dataFrame.header.frameIsPadded) {
             var sliceBuf = buffer.slice(dataFrame._section.offset,dataFrame._section.nextFrameIndex);
             var arr = [newBuffer,sliceBuf];
             newBuffer = Buffer.concat(arr);
         }
         else{
-            substituteHeaderBit(dataFrame,buffer,secret_message_arr[i].toString());
+            paddingStuff(dataFrame,buffer,secret_message_arr[i].toString());
             i++;
         }
-        dataFrame = mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex);
+         end padding stuff */
     }
 
     // open the file in writing mode, adding a callback function where we do the actual writing
@@ -85,6 +96,48 @@ fs.readFile(pathToMp3, (error, buffer) => {
 
 });
 
+//convert decimal to binary
+function convert_to_binary(text) {
+    var output = "";
+    for (var i = 0; i < text.length; i++) {
+        var string_no_padding = text[i].charCodeAt(0).toString(2);
+        var zero_char = '0';
+        var padding_string = zero_char.repeat(8 - string_no_padding.length);
+        padding_string += string_no_padding;
+        output += padding_string;
+    }
+    return output;
+}
+
+
+// convert binary to decimal
+function bin_to_dec(bstr) {
+    return parseInt((bstr + '')
+        .replace(/[^01]/gi, ''), 2);
+}
+
+function formatDecToBinary(value,format){
+    var binaryStr = null;
+    if(value>=0){
+        binaryStr = value.toString(2);
+        while(binaryStr.length < format) {
+            binaryStr = "0" + binaryStr;
+        }
+    }
+    else{
+        binaryStr = value.toString(2);
+        binaryStr = binaryStr.substring(1);
+        while(binaryStr.length < format) {
+            binaryStr = "0" + binaryStr;
+        }
+        binaryStr = "-" + binaryStr;
+    }
+
+
+    return binaryStr;
+
+}
+
 function paddingStuff(dataFrame, buffer, char) {
     var sliceBuf = buffer.slice(dataFrame._section.offset,dataFrame._section.nextFrameIndex);
     var b_dataview = new DataView(toArrayBuffer(sliceBuf));
@@ -95,17 +148,34 @@ function paddingStuff(dataFrame, buffer, char) {
     newBuffer = Buffer.concat(arr);
 }
 
-
-function substituteHeaderBit(dataFrame,buffer,bitchar){
-    var sliceBuf = buffer.slice(dataFrame._section.offset,dataFrame._section.nextFrameIndex);
-    var b_dataview = new DataView(toArrayBuffer(sliceBuf));
-    var value = b_dataview.getUint8(2) + 1;
-    b_dataview.setUint8(2,value);
-    var arr = [newBuffer,Buffer.from(b_dataview.buffer)];
-    newBuffer = Buffer.concat(arr);
+function invert_bit(bit_val){
+    if(bit_val==='1'){
+        return "0";
+    }
+    else{
+        return "1";
+    }
 }
 
-function check_max_size_embed_byte(dataFrame, message,buf_dataview){
+
+function check_max_embedbyte(dataFrame, message,buf_dataview){
+
+    while(mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex) != null) {
+        totframes++;
+        dataFrame = mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex);
+    }
+
+
+    if(totframes < secret_message.length * 8)
+        return false;
+
+    while (secret_message.length * 8 < totframes){
+        secret_message += "#";
+    }
+    return true
+}
+
+function check_max_embedbyte_unpadding(dataFrame, message,buf_dataview){
 
     while(mp3Parser.readFrame(buf_dataview,dataFrame._section.nextFrameIndex) != null) {
         totframes++;
@@ -125,19 +195,49 @@ function check_max_size_embed_byte(dataFrame, message,buf_dataview){
 
 
 
+function substituteHeaderBit(dataFrame,buffer,bitchar){
+    // recupero dataframe
+    let sliceBuf = buffer.slice(dataFrame._section.offset,dataFrame._section.nextFrameIndex);
+    let b_dataview = new DataView(toArrayBuffer(sliceBuf));
+    // recupero ultimo byte del dataframe
+    let lsb = b_dataview.getUint8(sliceBuf.length -1);
+    // recupero primi due bit più significativi
+    let msb = lsb.toString(2).substring(0,2);
 
+    //sostituisco private bit dell'header frame
+    if(msb == two_bit1){
+        let byte_header_str = formatDecToBinary(b_dataview.getUint8(2),8);
+        let bit_=byte_header_str.substring(0,byte_header_str.length-1);
+        let byte_modified = bit_ + invert_bit(bitchar);
+        let value_modified = bin_to_dec(byte_modified);
+        b_dataview.setUint8(2,value_modified);
+    }
+    else if(msb == two_bit2){
+        let byte_header_str = formatDecToBinary(b_dataview.getUint8(2),8);
+        let bit_=byte_header_str.substring(0,byte_header_str.length-1);
+        let byte_modified = bit_ + invert_bit(bitchar);
+        let value_modified = bin_to_dec(byte_modified);
+        b_dataview.setUint8(2,value_modified);
+    }
+    else if(msb == two_bit3){
+        let byte_header_str = formatDecToBinary(b_dataview.getUint8(3),8);
+        let bit_=byte_header_str.substring(0,byte_header_str.length-4);
+        let byte_modified = bit_ + invert_bit(bitchar) + byte_header_str.substring(5);
+        let value_modified = bin_to_dec(byte_modified);
+        b_dataview.setUint8(3,value_modified);
+    }
+    else if(msb == two_bit4){
+        let byte_header_str = formatDecToBinary(b_dataview.getUint8(3),8);
+        let bit_=byte_header_str.substring(0,byte_header_str.length-3);
+        let byte_modified = bit_ + invert_bit(bitchar) + byte_header_str.substring(6);
+        let value_modified = bin_to_dec(byte_modified);
+        b_dataview.setUint8(3,value_modified);
+    }
 
+    var arr = [newBuffer,Buffer.from(b_dataview.buffer)];
+    newBuffer = Buffer.concat(arr);
 
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -149,7 +249,7 @@ function substituteLSB(dataFrame, buffer) {
         if (value == -128)
             value = -127;
 
-            var binaryStr = formatDecToBinary(value);
+            var binaryStr = formatDecToBinary(value,15);
             var array = Array.from(binaryStr);
             array[array.length - 1] = Math.floor(Math.random() * (2 - 0));
             var binaryStr_altered = array.join("");
@@ -162,26 +262,6 @@ function substituteLSB(dataFrame, buffer) {
 
 
 
-function formatDecToBinary(value){
-    var binaryStr = null;
-    if(value>=0){
-        binaryStr = value.toString(2);
-        while(binaryStr.length < 15) {
-            binaryStr = "0" + binaryStr;
-        }
-    }
-    else{
-        binaryStr = value.toString(2);
-        binaryStr = binaryStr.substring(1);
-        while(binaryStr.length < 15) {
-            binaryStr = "0" + binaryStr;
-        }
-        binaryStr = "-" + binaryStr;
-    }
 
-
-    return binaryStr;
-
-}
 
 
